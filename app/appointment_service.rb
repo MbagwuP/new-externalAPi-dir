e1f6d359e310115db92d0b64c9486c3e35d9428d
“Peter <Plee@carecloud.com>
2013-08-12 15:54:20 -0400

    fixed syntax error

--- app/appointment_service.rb
+++ app/appointment_service.rb
@@ -336,7 +336,6 @@ class ApiService < Sinatra::Base
   # --> if not authorized: 401
   # --> if provider not found: 404
   # --> if exception: 500
-
   get '/v1/appointment/listbyid/:appointmentid?' do
 
     ## token management. Need unencoded tokens!
@@ -595,7 +594,7 @@ class ApiService < Sinatra::Base
 
       # iterate the array of appointments
       parsed.each { |x|
-        x['id'] = x[['external_id']
+        x['appointment']['id'] = x['appointment']['external_id']
       }
 
       LOG.debug(parsed)
