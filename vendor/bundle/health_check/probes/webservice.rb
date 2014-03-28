a8a23538653349e7672951fd91744eabc229509f
“Peter <Plee@carecloud.com>
2014-03-28 16:49:54 -0400

    changes to health check (webservice health check)

--- vendor/bundle/health_check/probes/webservice.rb
+++ vendor/bundle/health_check/probes/webservice.rb
@@ -1,10 +1,13 @@
 module Probes
   class Webservice < Probes::Probe
     def probe
-      if(CCloudWebServices)
-        is_up = false
-        conn = CCloudWebServices::WebService.health_check
-        is_up = true if conn== "200"
+      if(ApiService.settings.environment.to_s)
+        begin
+        conn = RestClient.get("#{ApiService.settings.api_url.to_s}/system/status_check")
+          is_up = true if conn.code== 200
+        rescue
+          is_up = false
+        end
         err_msg = "Service is down"
         record(*["Web Service", is_up, is_up ? "Web Service is active." : err_msg])
         self
