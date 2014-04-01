94385860e1907399d5516664456b7e7333132fe7
“Peter <Plee@carecloud.com>
2014-04-01 11:16:41 -0400

    Get settings from environment instead of from parent project

--- vendor/bundle/health_check/probes/mem_cache.rb
+++ vendor/bundle/health_check/probes/mem_cache.rb
@@ -2,11 +2,15 @@ module Probes
   class MemCache < Probes::Probe
 
     def probe
+      config_path = Dir.pwd + "/config/settings.yml"
+      config = YAML.load(File.open(config_path))[ENV['RACK_ENV']]
+      cache_url = config['memcache_servers']
 
-      if(ApiService.settings.cache)
+      if(cache_url)
         begin
-          ApiService.settings.cache.set("testvalue", "12346", 20)
-          newvalue = ApiService.settings.cache.get("testvalue")
+          cache = Dalli::Client.new(cache_url, :expires_in => 3600)
+          cache.set("testvalue", "12346", 20)
+          newvalue = cache.get("testvalue")
           cacheUp = true if (newvalue == "12346")
         rescue Exception => e
           cacheUp = false
