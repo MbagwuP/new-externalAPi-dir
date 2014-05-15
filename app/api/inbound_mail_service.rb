1de5276bf369ae226571bfae42ae86f8f086f0e3
jwalsh <jwalsh@carecloud.com>
2014-05-15 16:24:05 -0400

    Logging debug

--- app/api/inbound_mail_service.rb
+++ app/api/inbound_mail_service.rb
@@ -13,10 +13,14 @@ class ApiService < Sinatra::Base
         LOG.debug "Got send mail message"
         
         begin
-            LOG.debug request.body
-            
-            request_body = get_request_JSON
-            LOG.debug(request_body)
+            to = params['to']
+            cc = params['cc']
+            from = params['from']
+            subject = params['subject']
+            body = params['text']
+            num_attachments = params['attachments']
+            LOG.debug "To: #{to} CC: #{cc} From: #{from} Subject: #{subject} Body: #{body}"
+       
         rescue Exception => e
             LOG.error "Inbound_mail Error: #{e.message}"
         end
