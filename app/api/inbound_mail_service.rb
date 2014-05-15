2343f0d8e4c62349a0b49c79922cd563e373b456
jwalsh <jwalsh@carecloud.com>
2014-05-15 15:59:49 -0400

    test messages

--- app/api/inbound_mail_service.rb
+++ app/api/inbound_mail_service.rb
@@ -10,9 +10,18 @@ class ApiService < Sinatra::Base
     
     post '/inbound_mail' do
 
-        request_body = get_request_JSON
-        puts request_body
+        LOG.debug "Got send mail message"
+        
+        begin
+            LOG.debug request.body
+            
+            request_body = get_request_JSON
+            LOG.debug(request_body)
+        rescue Exception => e
+            LOG.error "Inbound_mail Error: #{e.message}"
+        end
 
+        status HTTP_OK
     end
 
 end
