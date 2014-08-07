f84a98d7994aa95cba6c958f6cf1ab9f6e683001
“Peter <Plee@carecloud.com>
2014-08-07 13:17:35 -0400

    Changes to webservice probe, to grab status from correct url

--- probes/webservice.rb
+++ probes/webservice.rb
@@ -2,8 +2,10 @@ module Probes
   class Webservice < Probes::Probe
     def probe
         begin
-        conn = RestClient.get("#{ApiService::SVC_URLS["api_internal_svc_url"]}system/status_check")
-          is_up = true if conn.code== 200
+        is_up = false
+        conn = RestClient.get("#{ApiService::SVC_URLS["api_internal_svc_url"]}health_check")
+        status = JSON.parse(conn)
+        is_up = true if status["service_status"] == 'up'
         rescue
           is_up = false
         end
