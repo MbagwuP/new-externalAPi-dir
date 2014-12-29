392fd7dae098325107082c25e0e29e7279ff05bb
Simon Bacquie <sbacquie@carecloud.com>
2014-12-29 10:29:21 -0500

    PLAT-589 More Swagger stuff: Disabled demo token endpoint for now, added Appointment Blockouts by Location endpoint, changed URLs to match conventions with other endpoints, cleaned up ERB file of broken resources that were being pulled in, clean method of re-using the base path for including CSS and JS files

--- app/api/swagger_service.rb
+++ app/api/swagger_service.rb
@@ -1,6 +1,6 @@
 class ApiService < Sinatra::Base
 
-  get '/api-docs/swagger.json' do
+  get '/v2/api-docs/swagger.json' do
     base = YAML.load_file 'api-docs/base.yml'
     base['paths'] = YAML.load_file 'api-docs/paths.yml'
     base['definitions'] = YAML.load_file 'api-docs/definitions.yml'
@@ -8,24 +8,22 @@ class ApiService < Sinatra::Base
     status HTTP_OK
   end
 
-  get '/api-docs' do
+  get '/v2/api-docs' do
     content_type :html
-    erb File.read 'public/api-docs/swagger.html'
+    erb File.read 'api-docs/swagger.erb'
   end
 
-  post '/api-docs/demotoken' do 
-=begin
+  post '/v2/api-docs/demotoken' do 
+    # Some thoughts on how this can work:
+    # - create token authorization request with Demo Business Entity and API Docs Application, and all OAuth roles (needs user token to authorize)
+    # - call oauth2/access_token with the authorization code to get the first (and only) token for this grant (needs basic auth with API key/secret to authorize)
 
-- create token authorization request with Demo BE and API Docs App, and all OAuth roles (needs user token to authorize)
-- call oauth2/access_token with the authorization code to get the first (and only) token for this grant (needs basic auth with API key/secret to authorize)
-
-=end
-
-    response = RestClient::Request.execute(:method => :post, :url => 'http://localhost:9292/oauth2/access_token',
-                  :user => 'mnQ2yOcOAGVRVL8VujUcC3TpufkueozK', :password => 'ly8JZ0RKyx9jQyLi',
-                  :payload => {grant_type: 'refresh_token', refresh_token: 'rEZKRVRrqM6S95yc_xNjhGS_UFvf0HH7'})
-    body response
-    status HTTP_OK
+    404 # disable this endpoint for now
+    # response = RestClient::Request.execute(:method => :post, :url => '/oauth2/access_token',
+    #               :user => 'mnQ2yOcOAGVRVL8VujUcC3TpufkueozK', :password => 'ly8JZ0RKyx9jQyLi',
+    #               :payload => {grant_type: 'refresh_token', refresh_token: 'rEZKRVRrqM6S95yc_xNjhGS_UFvf0HH7'})
+    # body response
+    # status HTTP_OK
   end
 
 end
