606796df332785323cb0c1e1b5a6e522b5a0f3ee
Simon Bacquie <sbacquie@carecloud.com>
2015-02-06 14:10:25 -0500

    PLAT-662 DRYed up list Visit Reasons endpoint

--- app/api/v2/visit_reasons.rb
+++ app/api/v2/visit_reasons.rb
@@ -1,26 +1,15 @@
 class ApiService < Sinatra::Base
 
+  # /v2/visit_reasons
+  # /v2/nature_of_visits (legacy)
   get /\/v2\/(nature_of_visits|visit_reasons)/ do
-    # /v2/visit_reasons
-    # /v2/nature_of_visits (legacy)
+    nature_of_visit_url = webservices_uri "nature_of_visits/list_by_business_entity/#{current_business_entity}.json",
+                                           token: escaped_oauth_token
 
-    nature_of_visit_url = ''
-    nature_of_visit_url << API_SVC_URL
-    nature_of_visit_url << 'nature_of_visits/list_by_business_entity/'
-    nature_of_visit_url << current_business_entity
-    nature_of_visit_url << '.json?token='
-    nature_of_visit_url << escaped_oauth_token
-
-    begin
-      response = RestClient.get(nature_of_visit_url, :api_key => APP_API_KEY)
-    rescue => e
-      begin
-        errmsg = "Nature Of Visit Look Up Failed - #{e.message}"
-        api_svc_halt e.http_code, errmsg
-      rescue
-        api_svc_halt HTTP_INTERNAL_ERROR, errmsg
-      end
+    response = rescue_service_call 'Visit Reason' do
+      RestClient.get(nature_of_visit_url, :api_key => APP_API_KEY)
     end
+
     data = []
     parsed = JSON.parse(response.body)
     parsed.each do |x|
@@ -32,7 +21,6 @@ class ApiService < Sinatra::Base
       end
     body(data.to_json)
     status HTTP_OK
-
   end
 
 end
