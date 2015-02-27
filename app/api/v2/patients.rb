33d26017ffc5bd124b389a605a999bf7041d7495
Simon Bacquie <sbacquie@carecloud.com>
2015-02-27 13:16:49 -0500

    Fixed bad regex match when visiting /v2/patients, should be 404 instead of 401

--- app/api/v2/patients.rb
+++ app/api/v2/patients.rb
@@ -58,7 +58,7 @@ class ApiService < Sinatra::Base
   # /v2/patients/{guid}
   # /v2/patients/{integer_id}
   get /\/v2\/patients\/(?<patient_id>([a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12})|[0-9]*)/ do |patient_id|
-    pass if params[:patient_id] == "search"
+    pass if params[:patient_id] == "search" || params[:patient_id].blank?
     begin
       patient_id = params[:patient_id]
       url   = "#{ApiService::API_SVC_URL}businesses/#{current_business_entity}/patients/#{patient_id}"
