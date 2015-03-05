3218666ffbc964d05758f3cf546b63791e7be24f
Simon Bacquie <sbacquie@carecloud.com>
2015-03-05 13:23:20 -0500

    PLAT-774 You can now list Nature of Visits for a BE by Resource ID

--- app/api/v2/visit_reasons.rb
+++ app/api/v2/visit_reasons.rb
@@ -23,4 +23,25 @@ class ApiService < Sinatra::Base
     status HTTP_OK
   end
 
+  get '/v2/appointment_resources/:resource_id/visit_reasons' do
+    nature_of_visit_url = webservices_uri "nature_of_visits/list_by_business_entity/#{current_business_entity}.json",
+                                           token: escaped_oauth_token, filter_resource_id: params[:resource_id]
+
+    response = rescue_service_call 'Visit Reason' do
+      RestClient.get(nature_of_visit_url, :api_key => APP_API_KEY)
+    end
+
+    data = []
+    parsed = JSON.parse(response.body)
+    parsed.each do |x|
+        filter_nov = {}
+        filter_nov['id'] = x['nature_of_visit']['id']
+        filter_nov['name'] = x['nature_of_visit']['name']
+        filter_nov['description'] = x['nature_of_visit']['description']
+        data.push filter_nov
+      end
+    body(data.to_json)
+    status HTTP_OK
+  end
+
 end
