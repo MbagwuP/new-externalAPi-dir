a21efdfb1e3615298d3fb257498395ef775e1ee0
“Jonathan <jsantiago@carecloud.com>
2015-12-18 19:22:27 -0500

    INT-1536 V1 endpoint for EMDAT transcription interface

--- app/api/encounter_service.rb
+++ app/api/encounter_service.rb
@@ -48,4 +48,47 @@ class ApiService < Sinatra::Base
     body(encounter_data.to_json)
     status HTTP_OK
   end
+
+
+
+  put '/v1/encounter_notes/:encounter_id/merge_transcriptions' do
+
+  request_body = get_request_JSON
+
+  pass_in_token = CGI::unescape(params[:authentication])
+
+  business_entity = get_business_entity(pass_in_token)
+
+  encounter_id = params[:encounter_id]
+
+    url_get_encounter_note = ''
+    url_get_encounter_note << API_SVC_URL
+    url_get_encounter_note << 'encounter_notes/'
+    url_get_encounter_note << business_entity.to_s
+    url_get_encounter_note << '/encounter_id/'
+    url_get_encounter_note << encounter_id.to_s
+    url_get_encounter_note << '/merge_transcriptions.json?token='
+    url_get_encounter_note << CGI::escape(pass_in_token)
+
+    begin
+    response = RestClient.put(url_get_encounter_note,request_body.to_json, :content_type => :json)
+    rescue => e
+      begin
+        api_svc_halt e.http_code, e.response.body
+      rescue
+        api_svc_halt HTTP_INTERNAL_ERROR, e.message
+      end
+    end
+
+    body(response)
+      
+    status HTTP_OK
+
+
+  end
+
+
+
+
+
 end
