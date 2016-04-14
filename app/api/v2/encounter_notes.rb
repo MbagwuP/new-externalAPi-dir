e1bb15eb9966f2192413a403fe246a5207b47fa0
Santiago Quintana <squintana@carecloud.com>
2016-04-14 17:16:46 -0400

    PLAT-1775 UPDATE call to webservices method

--- app/api/v2/encounter_notes.rb
+++ app/api/v2/encounter_notes.rb
@@ -1,13 +1,13 @@
-class ApiService << Sinatra::Base
+class ApiService < Sinatra::Base
 
-  put 'v2/encounter_notes/:encounter_id/merge_transcriptions' do
+  put '/v2/encounter_notes/:encounter_id/merge_transcriptions' do
 
     begin
-      access_token, encounter_id = get_oauth_token, params[:encounter_id]
+      access_token, encounter_id, business_entity_id = get_oauth_token, params[:encounter_id], params[:business_entity_id]
       request_body = get_request_JSON
       data  = CCAuth::OAuth2Client.new.authorization access_token
 
-      merge_transcription_url = "#{ApiService::API_SVC_URL}encounter_notes/#{data[:scope][:business_entity_id]}"
+      merge_transcription_url = webservices_uri "/encounter_notes/#{business_entity_id}"
       merge_transcription_url << "/encounter_id/#{encounter_id}/merge_transcriptions.json?token=#{access_token}"
 
       response = RestClient.put merge_transcription_url, request_body.to_json, :content_type => :json, extapikey: ApiService::APP_API_KEY
