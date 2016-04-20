1186f36b6940a35845a17211e1ea43ace141c71f
Santiago Quintana <squintana@carecloud.com>
2016-04-20 12:07:17 -0400

    PLAT-1775 Fix encounter_notes merge_transcriptions
    PLAT-1775 Delete oauth 2 client authorization

--- app/api/v2/encounter_notes.rb
+++ app/api/v2/encounter_notes.rb
@@ -3,14 +3,12 @@ class ApiService < Sinatra::Base
   put '/v2/encounter_notes/:encounter_id/merge_transcriptions' do
 
     begin
-      access_token, encounter_id, business_entity_id = get_oauth_token, params[:encounter_id], params[:business_entity_id]
+      encounter_id = params[:encounter_id]
       request_body = get_request_JSON
-      data  = CCAuth::OAuth2Client.new.authorization access_token
 
-      merge_transcription_url = webservices_uri "/encounter_notes/#{business_entity_id}"
-      merge_transcription_url << "/encounter_id/#{encounter_id}/merge_transcriptions.json?token=#{access_token}"
+      merge_transcription_uri = "/encounter_notes/#{business_entity_id}/encounter_id/#{encounter_id}/merge_transcriptions.json"
 
-      response = RestClient.put merge_transcription_url, request_body.to_json, :content_type => :json, extapikey: ApiService::APP_API_KEY
+      response = RestClient.put webservices_uri(merge_transcription_uri, token: escaped_oauth_token), request_body.to_json, :content_type => :json, extapikey: ApiService::APP_API_KEY
     rescue => e
       begin
         errmsg = "Merge transcriptions failed - #{e.message}"
