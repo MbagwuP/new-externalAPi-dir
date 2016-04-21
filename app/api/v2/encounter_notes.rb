fe4095c5730c7418694510d82a723ee5349055f7
Santiago Quintana <squintana@carecloud.com>
2016-04-21 16:00:41 -0400

    ADD merge transcription v2 endpoint PLAT-1775
    Built merge transcriptions endpoint following v2 conventions, using
    oauth2.
    
    Related to PLAT-1775 http://jira.carecloud.com/browse/PLAT-1775

--- app/api/v2/encounter_notes.rb
+++ app/api/v2/encounter_notes.rb
@@ -2,11 +2,15 @@ class ApiService < Sinatra::Base
 
   put '/v2/encounter_notes/:encounter_id/merge_transcriptions' do
 
+    
     begin
       encounter_id = params[:encounter_id]
       request_body = get_request_JSON
 
-      merge_transcription_uri = "/encounter_notes/#{business_entity_id}/encounter_id/#{encounter_id}/merge_transcriptions.json"
+      merge_transcription_url = webservices_uri(
+        "/encounter_notes/#{current_business_entity}/encounter_id/#{encounter_id}/merge_transcriptions.json",
+        token: escaped_oauth_token
+        )
 
       response = RestClient.put webservices_uri(merge_transcription_uri, token: escaped_oauth_token), request_body.to_json, :content_type => :json, extapikey: ApiService::APP_API_KEY
     rescue => e
@@ -21,5 +25,4 @@ class ApiService < Sinatra::Base
     body(parsed_response.to_json); status HTTP_OK
 
   end
-
 end
