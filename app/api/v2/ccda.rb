752baf570615a355334e6a6d4ec4dd609870ef9f
mgregory-carecloud <mgregory@carecloud.com>
2018-08-31 15:09:27 -0400

    ONC15-552/53/54/55/56/57 - Add External API Support for Ccda Requests
    * Added validations for handling ccda filters.

--- app/api/v2/ccda.rb
+++ app/api/v2/ccda.rb
@@ -1,18 +1,29 @@
 class ApiService < Sinatra::Base
 
+  # Options
+  ########################
+  # patient_id
+  # date
+  # start_date & end_date (YYYY-MM-DD)
+  # category
   post '/v2/ccda' do
-    ccda_url = webservices_uri(
-      "ccda.json",
+    params = get_request_JSON.symbolize_keys
+    command = ValidateAndBuildCreateCcdaParams.call(
+      params,
       token: escaped_oauth_token,
       business_entity_guid: current_business_entity
     )
-
-    response = rescue_service_call 'Create CCDA' do
-      RestClient.post(ccda_url, {})
+    if command.success?
+      ccda_url = webservices_uri("ccda.json", command.result)
+      response = rescue_service_call 'Create CCDA' do
+        RestClient.post(ccda_url, {})
+      end
+      body response
+      status HTTP_OK
+    else
+      api_svc_halt HTTP_BAD_REQUEST, command.errors
     end
 
-    body response
-    status HTTP_OK
   end
 
   get '/v2/ccda/:ccda_id' do
