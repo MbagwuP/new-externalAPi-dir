eca9e103ec2068843ce2110fceee400b961188ea
Prince Barai <princebarai@ip-192-168-0-168.ec2.internal>
2022-11-21 19:34:00 +0530

    EHRP-4728 code cleanup or care team end point

--- app/api/v2/care_team.rb
+++ app/api/v2/care_team.rb
@@ -1,15 +1,16 @@
 class ApiService < Sinatra::Base
 
   get '/v2/careteam/:id' do
-    care_team_id = params[:id]
 
+    care_team_id = params[:id]
     base_path ="care_team_members/#{care_team_id}/find_by_id.json"
+
     resp = evaluate_current_internal_request_header_and_execute_request(
       base_path: base_path,
       params: {},
       rescue_string: 'Care Team'
     )
-    puts "######## #{resp}"
+
     @careTeam = resp['care_team_members'].first    
     status HTTP_OK
     jbuilder :show_care_team
@@ -27,147 +28,11 @@ class ApiService < Sinatra::Base
       params: { status: care_team_status,},
       rescue_string: "Care team members"
     )
-    puts "response: #{resp}"
-
-#     resp = JSON.parse('{
-#     "care_team_members": [
-#         {
-#             "id": 3010241,
-#             "member_id": 1479960,
-#             "member_type": "Physician",
-#             "notes": null,
-#             "created_at": "2018-10-24T06:48:56-04:00",
-#             "effective_from": "2018-10-01T08:00:00-04:00",
-#             "effective_to": null,
-#             "person_relationship_type_id": null,
-#             "last_letter_sent_date": null,
-#             "default_letter_template_id": null,
-#             "original_id": null,
-#             "member_full_name": "ALBERT DAVIS",
-#             "member_guid": "PHYS_1479960",
-#             "business_entity": {
-#                 "id": 15480,
-#                 "name": "ONC15 DO NOT USE"
-#             },
-#             "patient": {
-#                 "id": 32524585,
-#                 "patient_number": "0068502-6970911100",
-#                 "full_name": "ALICE J NEWMAN PH.D",
-#                 "chart_number": "2362",
-#                 "external_id": "9e8a6b10-5e7f-4ace-840a-804183c64b9d",
-#                 "provider_id": "19796",
-#                 "be_id": "15480"
-#             },
-#             "provider": {
-#                 "id": 19796,
-#                 "npi": "1215980438"
-#             }
-#         },
-#         {
-#             "id": 3010242,
-#             "member_id": 24449866,
-#             "member_type": "Vo::Person",
-#             "notes": null,
-#             "created_at": "2018-10-24T06:53:54-04:00",
-#             "effective_from": "2018-10-01T08:00:00-04:00",
-#             "effective_to": null,
-#             "person_relationship_type_id": 9,
-#             "last_letter_sent_date": null,
-#             "default_letter_template_id": null,
-#             "original_id": null,
-#             "member_full_name": " ",
-#             "member_guid": "24449866",
-#             "business_entity": {
-#                 "id": 15480,
-#                 "name": "ONC15 DO NOT USE"
-#             },
-#             "patient": {
-#                 "id": 32524585,
-#                 "patient_number": "0068502-6970911100",
-#                 "full_name": "ALICE J NEWMAN PH.D",
-#                 "chart_number": "2362",
-#                 "external_id": "9e8a6b10-5e7f-4ace-840a-804183c64b9d",
-#                 "provider_id": "19796",
-#                 "be_id": "15480"
-#             },
-#             "provider": {
-#                 "id": 19796,
-#                 "npi": "1215980438"
-#             }
-#         },
-#         {
-#             "id": 3010243,
-#             "member_id": 24447892,
-#             "member_type": "Vo::Person",
-#             "notes": null,
-#             "created_at": "2018-10-24T06:55:01-04:00",
-#             "effective_from": "2018-10-01T08:00:00-04:00",
-#             "effective_to": null,
-#             "person_relationship_type_id": 2,
-#             "last_letter_sent_date": null,
-#             "default_letter_template_id": null,
-#             "original_id": null,
-#             "member_full_name": " ",
-#             "member_guid": "24447892",
-#             "business_entity": {
-#                 "id": 15480,
-#                 "name": "ONC15 DO NOT USE"
-#             },
-#             "patient": {
-#                 "id": 32524585,
-#                 "patient_number": "0068502-6970911100",
-#                 "full_name": "ALICE J NEWMAN PH.D",
-#                 "chart_number": "2362",
-#                 "external_id": "9e8a6b10-5e7f-4ace-840a-804183c64b9d",
-#                 "provider_id": "19796",
-#                 "be_id": "15480"
-#             },
-#             "provider": {
-#                 "id": 19796,
-#                 "npi": "1215980438"
-#             }
-#         },
-#         {
-#             "id": 3010244,
-#             "member_id": 24447893,
-#             "member_type": "Vo::Person",
-#             "notes": null,
-#             "created_at": "2018-10-24T06:55:31-04:00",
-#             "effective_from": "2018-10-01T08:00:00-04:00",
-#             "effective_to": null,
-#             "person_relationship_type_id": 6,
-#             "last_letter_sent_date": null,
-#             "default_letter_template_id": null,
-#             "original_id": null,
-#             "member_full_name": " ",
-#             "member_guid": "24447893",
-#             "business_entity": {
-#                 "id": 15480,
-#                 "name": "ONC15 DO NOT USE"
-#             },
-#             "patient": {
-#                 "id": 32524585,
-#                 "patient_number": "0068502-6970911100",
-#                 "full_name": "ALICE J NEWMAN PH.D",
-#                 "chart_number": "2362",
-#                 "external_id": "9e8a6b10-5e7f-4ace-840a-804183c64b9d",
-#                 "provider_id": "19796",
-#                 "be_id": "15480"
-#             },
-#             "provider": {
-#                 "id": 19796,
-#                 "npi": "1215980438"
-#             }
-#         }
-#     ]
-# }')
 
     @care_team_members = resp['care_team_members']
     @include_provenance_target = params[:_revinclude] == 'Provenance:target' ? true : false
 
-    puts "@include_provenance_target #{@include_provenance_target}"
     status HTTP_OK
-    # jbuilder :show_care_team
     jbuilder :list_care_team
   end
 end
--- app/api/views/show_care_team.jbuilder
+++ app/api/views/show_care_team.jbuilder
@@ -1,107 +1,58 @@
-# first_care_team_member = OpenStruct.new(@care_team_members.first)
-# patient = OpenStruct.new(first_care_team_member.patient)
-# business_entity = OpenStruct.new(first_care_team_member.business_entity)
+careTeam = OpenStruct.new(@careTeam)
+patient = OpenStruct.new(careTeam.patient)
+business_entity = OpenStruct.new(careTeam.business_entity)
+provider = OpenStruct.new(careTeam.provider)
 
-# json.care_team do
-#   json.status status_by_any_active_participant(@care_team_members)
+json.careTeam do
+  json.account_number patient.external_id
+  json.mrn patient.chart_number
+  json.patient_name patient.full_name
+  json.identifier careTeam.id
+  json.text "Care Team"
+  json.text_status "generated"
+  json.status status_by_dates(careTeam.effective_from, careTeam.effective_end)
+  json.period_start careTeam.effective_from
+  json.period_end careTeam.effective_end
 
-#   json.text 'Care Team'
-#   json.text_status 'generated'
-
-#   json.participant @care_team_members do |ctm|
-#     role = participant_role(ctm['member_type'])
-
-#     json.id ctm['member_guid']
-#     json.role role
-
-#     if role == 'Physician'
-#       json.code '158965000'
-#       json.code_display 'snomed'
-#       json.code_system 'Medical Practicioner'
-#     else
-#       json.code ''
-#       json.code_display ''
-#       json.code_system ''
-#     end
-
-#     json.member ctm['member_full_name']
-#     json.period_start ctm['effective_from']
-#     json.period_end ctm['effective_end']
-#   end
-  
-#   json.patient do
-#     json.partial! :patient, patient: patient
-#   end
-  
-#   json.business_entity do
-#     json.partial! :business_entity, business_entity: business_entity
-#   end
-# end
-
-
-json.careTeamEntries @care_team_members do |careTeam|
-
-  careTeam = OpenStruct.new(careTeam)
-  patient = OpenStruct.new(careTeam.patient)
-  business_entity = OpenStruct.new(careTeam.business_entity)
-  provider = OpenStruct.new(careTeam.provider)
-
-  json.careTeam do
-
-    json.account_number patient.external_id
-    json.mrn patient.chart_number
-    json.patient_name patient.full_name
-    json.identifier careTeam.id
-    json.text "Care Team"
-    json.text_status "generated"
-    json.status status_by_dates(careTeam.effective_from, careTeam.effective_end)
-    json.period_start careTeam.effective_from
-    json.period_end careTeam.effective_end
-
-    json.participant do
-      json.array!([:once]) do
-        role = participant_role(careTeam['member_type'])
-        json.role do
-          json.coding do
-            json.array!([:once]) do
-              if role == 'Physician'
-                  json.code '158965000'
-                  json.code_display 'snomed'
-                  json.code_system 'Medical Practicioner'
-              else
-                  json.code '116154003'
-                  json.code_display 'snomed'
-                  json.code_system 'Patient'
-              end
+  json.participant do
+    json.array!([:once]) do
+      role = participant_role(careTeam['member_type'])
+      json.role do
+        json.coding do
+          json.array!([:once]) do
+            if role == 'Physician'
+                json.code '158965000'
+                json.code_display 'snomed'
+                json.code_system 'Medical Practicioner'
+            else
+                json.code '116154003'
+                json.code_display 'snomed'
+                json.code_system 'Patient'
             end
           end
-          if role == 'Physician'
-            json.text 'Medical Practicioner'
-          else
-            json.text 'Patient'
-          end
-          
         end
-        json.member do
-          if role == 'Physician'
-            json.reference "Practitioner/#{careTeam['member_id']}"
-            json.display careTeam['member_full_name']
-          else
-            json.reference "Patient/#{patient.external_id}"
-            json.display patient.full_name
-          end
-        end    
+        if role == 'Physician'
+          json.text 'Medical Practicioner'
+        else
+          json.text 'Patient'
+        end
+        
       end
-    end
-    
-    json.healthcare_entity do
-      json.partial! :healthcare_entity, healthcare_entity: business_entity
-    end
-
-    if @include_provenance_target
-	    json.partial! :_provenance, patient: patient, record: careTeam, 
-      provider: provider, business_entity: business_entity, obj: 'CareTeam'
+      json.member do
+        if role == 'Physician'
+          json.reference "Practitioner/#{careTeam['member_id']}"
+          json.display careTeam['member_full_name']
+        else
+          json.reference "Patient/#{patient.external_id}"
+          json.display patient.full_name
+        end
+      end    
     end
   end
+  
+  json.healthcare_entity do
+    json.partial! :healthcare_entity, healthcare_entity: business_entity
+  end
 end
+
 
