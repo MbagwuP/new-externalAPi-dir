502fe0562ead394a59e7891644bd3eb6e83220a0
15174 - Tahir Ahmed 3 <CARECLOUD\tahmed3@a-3vgk22rcm5mnj.cloud.carecloud.com>
2022-12-02 15:40:03 +0500

    code for category

--- app/api/v2/observations.rb
+++ app/api/v2/observations.rb
@@ -3,7 +3,7 @@ class ApiService < Sinatra::Base
   get '/v2/observations' do
     patient_id = params[:patient_id]
     validate_patient_id_param(patient_id)
-    code_for_path = params[:category] ? "5778-6" : params[:code] 
+    code_for_path = params[:category] == "laboratory" ? "5778-6" : params[:code] 
     base_path = get_observations_path(code_for_path)
     code = get_observations_code(params[:code])
     parameters = { patient_id: patient_id, code: code, date: params[:date], ccd_components: ['social_history'] }
