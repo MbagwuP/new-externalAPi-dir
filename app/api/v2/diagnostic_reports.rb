a23691a77edd8a2a7df0b91c5a33b546ab2da699
Prince Barai <princebarai@ip-192-168-0-168.ec2.internal>
2022-12-23 21:36:49 +0530

    EHRP-5521-handled null-exception for lab results

--- app/api/v2/diagnostic_reports.rb
+++ app/api/v2/diagnostic_reports.rb
@@ -25,23 +25,27 @@ class ApiService < Sinatra::Base
     end
 
     @lab_result = resp["lab_results"]
-    @patient = OpenStruct.new resp["patient"]["patient"]
-    @provider = OpenStruct.new resp["provider"]["provider"]
-    @business_entity = OpenStruct.new resp["business_entity"]["business_entity"]
-    base_path1 = "patient_summary/generate_json_by_patient_id_and_component.json"
+    if !@lab_result.empty?
 
-    resp1 = evaluate_current_internal_request_header_and_execute_request(
-      base_path: base_path1,
-      params: { patient_id: @patient.external_id, ccd_components: ['labresults'], code: params[:code], category: params[:category],date: params[:date]},
-      rescue_string: "Diagnostic report "
-    )
-    patient_summary = resp1['patient_summary']
-    @encounter = resp1['encounter']['encounter']
-    patient_summary = JSON.parse(patient_summary) if patient_summary
-    diagnostic_reports_section = patient_summary['ClinicalDocument']['component']['structuredBody']['component']['section']
+      @patient = OpenStruct.new resp["patient"]["patient"]
+      @provider = OpenStruct.new resp["provider"]["provider"]
+      @business_entity = OpenStruct.new resp["business_entity"]["business_entity"]
+      base_path1 = "patient_summary/generate_json_by_patient_id_and_component.json"
+
+      resp1 = evaluate_current_internal_request_header_and_execute_request(
+        base_path: base_path1,
+        params: { patient_id: @patient.external_id, ccd_components: ['labresults'], code: params[:code], category: params[:category],date: params[:date]},
+        rescue_string: "Diagnostic report "
+      )
+      patient_summary = resp1['patient_summary']
+      @encounter = resp1['encounter']['encounter']
+      patient_summary = JSON.parse(patient_summary) if patient_summary
+      diagnostic_reports_section = patient_summary['ClinicalDocument']['component']['structuredBody']['component']['section']
 
 
-    @diagnostic_report = ResultSection.new(diagnostic_reports_section)
+
+      @diagnostic_report = ResultSection.new(diagnostic_reports_section)
+    end
 
     @code = "5778-6"
 
@@ -68,7 +72,7 @@ class ApiService < Sinatra::Base
     @include_category_target = params[:category] || nil
     @include_date_target = params[:date] || nil
     patient_summary = resp['patient_summary']
-
+    
     patient_summary = JSON.parse(patient_summary) if patient_summary
     diagnostic_reports_section = patient_summary['ClinicalDocument']['component']['structuredBody']['component']['section']
 
@@ -82,25 +86,27 @@ class ApiService < Sinatra::Base
     # (@include_category_target == default_category_code || @include_category_target == nil) &&
     # (@include_date_target == test_date || @include_date_target == nil))
 
-    @business_entity = resp['business_entity']['business_entity']
-    @encounter = resp['encounter']['encounter']
-    @provider = resp['provider']['provider']
-    @include_provenance_target = params[:_revinclude] == 'Provenance:target' ? true : false
+    if !@lab_requests.empty?
+      @business_entity = resp['business_entity']['business_entity']
+      @encounter = resp['encounter']['encounter']
+      @provider = resp['provider']['provider']
+      @include_provenance_target = params[:_revinclude] == 'Provenance:target' ? true : false
 
-    @lab_requests.each do |lab_request|
-      document_hash = lab_request['lab_request_test']['document']
-      document_url = document_hash ? document_hash['document_url'] : nil
-      if document_url
-        begin
-          internal_signed_request = sign_internal_request(url: document_url, method: :get, headers: {accept: :json})
-          data = internal_signed_request.execute
-        rescue => e
+      @lab_requests.each do |lab_request|
+        document_hash = lab_request['lab_request_test']['document']
+        document_url = document_hash ? document_hash['document_url'] : nil
+        if document_url
+          begin
+            internal_signed_request = sign_internal_request(url: document_url, method: :get, headers: {accept: :json})
+            data = internal_signed_request.execute
+          rescue => e
+            data=nil
+          end
+        else
           data=nil
         end
-      else
-        data=nil
+        lab_request['lab_request_test']['data'] = data
       end
-      lab_request['lab_request_test']['data'] = data
     end
 
     status HTTP_OK
