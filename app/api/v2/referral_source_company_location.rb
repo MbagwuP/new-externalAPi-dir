dd7f832a9eff1781780a39ff5839bb495203cfee
muhammadraihan-carecloud <123186406+muhammadraihan-carecloud@users.noreply.github.com>
2023-05-23 22:19:56 +0500

    Cen 2992 (#867)
    * CEN-2992 Update Referral Physician and Referral Locations External API
    
    * CEN-2992 Code re-factor with the help of Rubocop
    
    * CEN-2992 If condition replaced with unless
    
    * CEN-2992 validate method code refactor
    
    ---------
    
    Co-authored-by: 16111 - Muhammad Raihan <CARECLOUD\mraihan@a-245in03g8z9n8.cloud.carecloud.com>

--- app/api/v2/referral_source_company_location.rb
+++ app/api/v2/referral_source_company_location.rb
@@ -31,10 +31,10 @@ class ApiService < Sinatra::Base
   post '/v2/referral_location/create' do
     request_body = get_request_JSON
 
-    error_message = validate_phone_and_fax(request_body)
-    if error_message 
-      errmsg = "Referral Location Creation Failed - The phones array must contain 2 elements. A phone number for a fax machine, and a phone number associated to the location"
-      api_svc_halt HTTP_BAD_REQUEST, errmsg
+    unless validate(request_body)
+      api_svc_halt(HTTP_BAD_REQUEST, %(
+        The phones array requires 2 elements: a fax number and a location-associated number.
+      ))
     end
 
     @state_id = WebserviceResources::Converter.code_to_cc_id(WebserviceResources::State, request_body['address']['state_code'])
@@ -125,23 +125,11 @@ post '/v2/referral_source/create' do
   status HTTP_CREATED
 end
 
-private
+  private
 
-def validate_phone_and_fax(request_body)
-  phone_type_codes = []
-  fax = nil
-  request_body["phones"].each do |phone|
-    if phone["phone_type_code"] == "M" or phone["phone_type_code"] == "P" or phone["phone_type_code"] == "C" or phone["phone_type_code"] == "H" or phone["phone_type_code"] == "B"
-      phone_type_codes << phone["phone_type_code"]
-    elsif phone["phone_type_code"] == "F"
-      fax = phone
-    end
+  def validate(req)
+    phone_type_codes = req['phones'].map { |phone| phone['phone_type_code'] }
+    phone_type_codes.include?('F') && phone_type_codes.any? { |code| %w[M P C H B].include?(code) }
   end
- 
-  if fax.nil?
-    return "Fax is missing"
-  elsif phone_type_codes.empty?
-    return "Phone is missing"
-  end
-end
+
 end
